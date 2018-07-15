74c21edc30a3f315d39a2b7d732c4ffe2e586e6a
daikikojima <daikikojima0531@icloud.com>
2018-07-15 23:48:12 +0900

    Initialize commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method=="POST":
+        img = request.files['img']
+        if img and allowed_file(img.filename):
+            filename = secure_filename(img.filename)
+            img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img_url = '/uploads/' + filename
+            return render_template('result.html',img_url = img_url)
+
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
